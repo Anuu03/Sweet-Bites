188a7ab777107c3f4c36393dd430c2dbd4393766
DubeyJi03 <dubeyanurag3925@gmail.com>
2026-02-15 03:10:38 +0530

    change

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL =
-    import.meta.env.VITE_BACKEND_URL ||
-    "https://sweet-bites-8x2l.onrender.com";
+
+
+const API_BASE_URL = "https://sweet-bites-8x2l.onrender.com";
 
 export default API_BASE_URL;
